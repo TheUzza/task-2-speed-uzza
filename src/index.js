879e810a60c12d617d70ff0ccec34b05e79e1663
Uzza <salamsyr12213@gmail.com>
2023-01-19 08:07:37 +0100

    Please work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+  </BrowserRouter>
+);
+
+
+reportWebVitals();
